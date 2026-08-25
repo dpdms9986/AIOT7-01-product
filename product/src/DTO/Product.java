659346f912bac0a51335aa1ce464c22ad0fa57e2
최재아 <lanachan1212@gmail.com>
2026-08-25 15:40:31 +0900

    Add Product class and VSCode settings for Java project

---
+++ product/src/DTO/Product.java
@@ -0,0 +1,37 @@
+package DTO;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+// 게시글 정보 객체 정의하기
+// 글번호, 제목, 작성자, 내용, 등록일자, 수정일자
+
+@Data
+@AllArgsConstructor
+
+public class Product {
+
+    // 상품의 번호(몇번째로 들어왔는지 혹은 상품코드)
+    private int no;
+    // 상품명
+    private String name;
+    // 유통기한
+    private String exdate;
+    // 등록날짜
+    private Date createdAt;
+    // 수정일
+    private Date updatedAt;
+    
+    
+    // 기본생성자
+    public Product() {
+        this(" [이름없음] ", " 0000/00/00 " );
+
+    }
+
+    public Product(String name, String exdate){
+        this.name = name;
+        this.exdate = exdate;
+    }
+}
