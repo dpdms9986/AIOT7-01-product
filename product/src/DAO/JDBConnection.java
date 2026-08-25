22f50d00f60a371e97e7cbfe7694479576a1f1a7
dpdms9986 <dpdms9987@gmail.com>
2026-08-25 17:51:17 +0900

    진짜 오류 수정

--- product/src/DAO/JDBConnection.java
+++ product/src/DAO/JDBConnection.java
@@ -29,7 +29,7 @@ public class JDBConnection {
 			//    * 11g  : xe
 			//    * 12c~ : orcl
 			String url = "jdbc:oracle:thin:@localhost:1521:orcl2";
-			String id = "system";
+			String id = "aloha";
 			String pw = "123456";
 			
 			// 자바 프로그램에서 JDBC 드라이버를 연결시켜주는 클래스
